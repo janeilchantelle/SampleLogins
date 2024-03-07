a35760d0062bf05cada787e47ec438169d64c038
Janeil Carroll <janeilchantelle@gmail.com>
2024-03-07 10:25:25 -0330

    Adding DAL files

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ const app = express();
 const PORT = 3000;
 
 global.DEBUG = true;
-app.set('view')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true, })); // This is important!
+app.use(methodOverride('_method')); // So is this!
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { name: 'Peter'});
+});
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+app.listen(PORT, () => {
+  console.log(`Simple app running on port ${PORT}.`)
+});
